17e26f07aa36424699ec1ea2fb06d09ce6cd58d0
Roman Carrubba <roman.carrubba@gmail.com>
2025-01-09 18:26:21 -0800

    added constructorVector3D

--- Vector3D.java
+++ Vector3D.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Vector3D {
+    private double x;
+    private double y;
+    private double z;
+
+    public Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+
+
+
+
 }
